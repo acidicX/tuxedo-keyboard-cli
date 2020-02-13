0c2ffacf84693cded5d8f00bf6d3997a08989fc2
Carsten <carsten.meier@peerigon.com>
2020-02-13 17:37:58 +0100

    add state and check color formatting

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,13 @@ error_chain! {
             description("kernel module not loaded or lsmod failed")
             display("kernel module not loaded or lsmod failed")
         }
-        WrongCliParamRange(t: String) {
-            description("a cli param has been specified with the wrong range")
-            display("the cli param has been specified with the wrong range - outside '{}'", t)
+        WrongCliOptionRange(t: String) {
+            description("a cli option has been specified with the wrong range")
+            display("the cli option has been specified with the wrong range - outside '{}'", t)
+        }
+        WrongCliColor(t: String) {
+            description("the color param has been wrongly specified")
+            display("the color param has been wrongly specified, must be an RGB hex string like 'FF0000' - input: '{}'", t)
         }
         ShellExecFailed(t: String) {
             description("shell exec has failed")
@@ -35,10 +39,39 @@ error_chain! {
 fn validate_mode(mode: &str) -> Result<u8> {
     let int_mode = mode.parse::<u8>().unwrap();
     if int_mode > 7 {
-        bail!(ErrorKind::WrongCliParamRange("0 - 7".to_string()));
+        bail!(ErrorKind::WrongCliOptionRange("0 - 7".to_string()));
     }
 
     Ok(int_mode)
+}
+
+fn validate_state(state: &str) -> Result<u8> {
+    let parsed_state = state.parse::<u8>();
+    let int_state = match parsed_state {
+        Ok(int_state) => int_state,
+        Err(_error) => bail!(ErrorKind::WrongCliOptionRange("0 - 1".to_string())),
+    };
+
+    if int_state > 1 {
+        bail!(ErrorKind::WrongCliOptionRange("0 - 1".to_string()));
+    }
+
+    Ok(int_state)
+}
+
+fn validate_color(color: &str) -> Result<String> {
+    if color.len() != 6 {
+        bail!(ErrorKind::WrongCliColor(color.to_string()));
+    }
+
+    let color_result = u32::from_str_radix(color, 16).map(|o| o as u32);
+
+    match color_result {
+        Ok(color) => color,
+        Err(_error) => bail!(ErrorKind::WrongCliColor(color.to_string())),
+    };
+
+    Ok(color.to_string())
 }
 
 #[derive(StructOpt, Debug)]
@@ -48,15 +81,20 @@ fn validate_mode(mode: &str) -> Result<u8> {
 )]
 struct Cli {
     /// color as RGB string
+    #[structopt(parse(try_from_str = validate_color))]
     color: String,
 
     /// brightness (0 - 255)
-    #[structopt(long = "brightness", short = "b", default_value = "75")]
+    #[structopt(long = "brightness", short = "b", default_value = "255")]
     brightness: u8,
 
     /// backlight modes (0 - 7)
     #[structopt(long = "mode", short = "m", default_value = "0", parse(try_from_str = validate_mode))]
     mode: u8,
+
+    /// state (0 or 1)
+    #[structopt(long = "state", short = "s", default_value = "1", parse(try_from_str = validate_state))]
+    state: u8,
 }
 
 fn preflight_check() -> Result<()> {
@@ -158,6 +196,12 @@ fn run() -> Result<()> {
         "echo 0x", cli.color, " > /sys/devices/platform/tuxedo_keyboard/color_extra"
     );
     exec_sh(set_color_extra_str)?;
+
+    let set_state_str = format!(
+        "{}{}{}",
+        "echo ", cli.state, " > /sys/devices/platform/tuxedo_keyboard/state"
+    );
+    exec_sh(set_state_str)?;
 
     Ok(())
 }
